8badcf6dc6e6481d49b74e863a497ffe7d14c263
Victor <vicalle49@gmail.com>
2023-04-02 14:00:52 +0200

    fix type

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Layout, Text, Page } from '@vercel/examples-ui'
 import { Chat } from '../components/Chat'
 import {GetServerSidePropsContext} from "next";
 
-function Home({authValid}) {
+function Home({authValid}: {authValid: boolean}) {
   return (
     <Page className="flex flex-col gap-12">
         {authValid ? <Chat /> : <Text className="text-zinc-600">You are not authorized to use this app</Text>}
